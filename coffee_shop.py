8328c48eea54be9841e7afca4af07ed585a7ddaa
SavinduPerera <savindunimsara222@gmail.com>
2025-11-11 17:39:56 +0530

    Add files via upload

---
+++ coffee_shop.py
@@ -0,0 +1,10 @@
+print("welcome to Caffeya Coffee Shop......")
+
+name=input("What is your name? ")
+
+print("Hello ",name + " thank you so much for comming in today!!!!! \n\n" )
+
+menu = "Black coffee, Espresso, Latte, Cappucino"
+print(menu)
+order=input("What do you like to order? ")
+print("sounds good " + name + " we'll have that " + order + ", ready for you in a moment.")
